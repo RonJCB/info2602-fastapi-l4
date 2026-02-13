0f38d519b152c4c28502ae37bdc909e8a4861639
RonJCB <ronellbaptiste30@gmail.com>
2026-02-13 12:24:58 -0400

    Update authCategory.py
    pass specific parameters(todo id and category id instead of the entire object

--- app/routers/authCategory.py
+++ app/routers/authCategory.py
@@ -29,15 +29,15 @@ async def create_category(db:SessionDep, user:AuthDep, cat: Category):
 #add a category reponse model to see changes on client
 #Assigns the category cat_id to the todo todo_id if the user is authorized to access it
 @catUser_router.post("/todo/{todo_id}/category/{cat_id}", response_model = TodoCategory)
-async def add_category(db:SessionDep, user:AuthDep, category:Category):
-         todo = db.exec(select(Todo).where(Todo.user_id == user.id )).one_or_none()
+async def add_category(db:SessionDep, user:AuthDep, category_id:int, todo_id:int):
+         todo = db.exec(select(Todo).where(Todo.id == todo_id)).one_or_none()
          if todo:#if find dthaat todo with category id assignment
               raise HTTPException(
                    status_code = status.HTTP_404_UNAUTHORIZED,
                    details = "Todo with that category Id already found in database"
                    
               )
-         cat = db.exec(select(Category).where(Category.id == category.id)).one_or_none()
+         cat = db.exec(select(Category).where(Category.id == category_id)).one_or_none()
          if cat:
                raise HTTPException(
                      status_code = status.HTTP_404_NOT_FOUND,
@@ -47,8 +47,8 @@ async def add_category(db:SessionDep, user:AuthDep, category:Category):
         
          #create that todocategory listing if not there
          new_Category = TodoCategory(
-            category_id = category.id
-            ,todo_id = todo.id
+            category_id = category_id
+            ,todo_id = todo_id
         )
          
          db.add(new_Category)
@@ -57,33 +57,33 @@ async def add_category(db:SessionDep, user:AuthDep, category:Category):
          return new_Category
 
 @catUser_router.delete("/todo/{todo_id}/category/{cat_id}")
-def delete_Category(db:SessionDep, user:AuthDep, cat:Category):
-             todo = db.exec(select(Todo).where(Todo.user_id == user.id)).one_or_none()
+def delete_Category(db:SessionDep, user:AuthDep, cat_id:int, todo_id:int):
+             todo = db.exec(select(Todo).where(Todo.id ==todo_id)).one_or_none()
              if not todo:
                 raise HTTPException(
                         status_code= status.HTTP_404_NOT_FOUND,
                         details ="Todo with that category assignment not found in db",
                 )
-             category = db.exec(select(Category).where(Category.id == cat.id)).one_or_none()
+             category = db.exec(select(Category).where(Category.id == cat_id)).one_or_none()
              if not category:
                    raise HTTPException(
                          status_code= status.HTTP_404_NOT_FOUND,
                         details ="Todo with that category assignment not found in db",   
                    )
-             todoCat = db.exec(select(TodoCategory).where(TodoCategory.todo_id == todo.id
-                                                          , TodoCategory.category_id == category.id))
+             todoCat = db.exec(select(TodoCategory).where(TodoCategory.todo_id == todo_id
+                                                          , TodoCategory.category_id == cat_id))
              db.delete(todoCat)
              db.commit()
 
 @catUser_router.get("/category/{cat_id}/todos")
-async def get_Todos(db:SessionDep, user:AuthDep, cat:Category):
-            getTodoByCategory = db.exec(select(Category).where(Category.user_id == user.id, Category.id == cat.id)).all()
+async def get_Todos(db:SessionDep, user:AuthDep, cat_id:int):
+            getTodoByCategory = db.exec(select(Category).where(Category.user_id == user.id, Category.id == cat_id)).all()
             if not getTodoByCategory:
                   raise HTTPException(
                         status_code = status.HTTP_404_NOT_FOUND,
                         details = "Todos Categories by that ID not found in db",
                   )
-            getTodos = db.exec(select(Todo).where(Todo.user_id == user.id, getTodoByCategory.id == cat.id)).all()
+            getTodos = db.exec(select(Todo).where(Todo.user_id == user.id, getTodoByCategory.id == cat_id)).all()
 
             if not getTodos:
                 raise HTTPException(
